004df156cb941987839207b45d3674b1789271e2
Ed Hawkins <96295230+ehawkins18@users.noreply.github.com>
2022-10-23 20:30:54 +0000

    updated final project

---
+++ Final_Project/rockPaperScissorsStarterCode.py
@@ -0,0 +1,15 @@
+#Rock, Paper, Scissors (Starter Code) 
+#By Fearsome Foursome - Wayne Tobias, Usman Ahmed, Kirk Martin, Ed Hawkins
+
+#Import random number generator
+
+#Ask player to choose Rock, Paper, or Scissors (default is Rock)   
+
+#Print out player choice
+
+#Have computer pick Rock, Paper, or Scissors
+
+#Output computer's choice
+
+#Check for winner and output results
+    
