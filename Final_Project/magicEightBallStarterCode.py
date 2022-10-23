87f7f58b54d31c0a01f6f60904aa5ac4369da5fe
Ed Hawkins <96295230+ehawkins18@users.noreply.github.com>
2022-10-23 21:09:05 +0000

    updated final project

---
+++ Final_Project/magicEightBallStarterCode.py
@@ -0,0 +1,16 @@
+#Magic Eight Ball (Starter Code) 
+#By Fearsome Foursome - Wayne Tobias, Usman Ahmed, Kirk Martin, Ed Hawkins
+
+#Import random number generator
+
+#Create variables for responses
+
+#Create list of responses
+    
+#Ask player to input a question   
+  
+#Have computer pick Magic 8 Ball response
+
+#Show Response Number while testing/comment out for game play
+
+#Output response
